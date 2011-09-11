cd44c7938e5b951cb03a17237e74f4048754da45
Matt Stockwell <matt.stockwell@optusnet.com.au>
2011-09-11 22:45:53 +1000

    fix typos

--- gen/com/ideos/x5settings/R.java
+++ gen/com/ideos/x5settings/R.java
@@ -48,7 +48,7 @@ public final class R {
         public static final int app_name=0x7f040000;
         public static final int complete=0x7f040002;
         public static final int complete_reboot=0x7f040003;
-        public static final int cpu_covemor=0x7f040006;
+        public static final int cpu_governor=0x7f040006;
         public static final int dup_filter=0x7f040005;
         public static final int enable_feedback=0x7f040001;
         public static final int exit=0x7f040004;
